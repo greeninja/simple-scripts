40828f0d6f3dbf8147e552891330e4f965dd28f2
Nick Campion <nick.campion@fasthosts.com>
2017-01-16 13:03:37 +0000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#### Some simple scripts to make life easier
---
+++ bash/wordpress/wptest-io.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installs wp-cli as a php script, installs all tests from http://wptest.io and cleans up after itself.
+# Assumes web dir is /var/www/html
+
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -OL https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
+# Install import plugin
+php wp-cli.phap plugin install wordpress-importer --activate
+# Now install all the stuff from http://wptest.io
+php wp-cli.phar import wptest.xml --authors=create
+
+# Now clean up
+rm wptest.xml
+rm wp-cli.phar
