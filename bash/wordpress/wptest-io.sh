f76e1ff63a332afa1cc87cbc75a6cb08898c130c
Nick Campion <nick.campion@fasthosts.com>
2017-01-16 13:57:51 +0000

    additions

--- bash/wordpress/wptest-io.sh
+++ bash/wordpress/wptest-io.sh
@@ -8,6 +8,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 curl -OL https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
 # Install import plugin
 php wp-cli.phar plugin install wordpress-importer --activate
+php wp-cli.phar plugin install jetpack --activate
 # Now install all the stuff from http://wptest.io
 php wp-cli.phar import wptest.xml --authors=create
 
