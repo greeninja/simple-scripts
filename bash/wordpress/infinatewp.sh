a886a838370832585e8a24c67434354e0b283c28
greeninja <nick@greeninja.co.uk>
2017-01-18 11:09:56 +0000

    add user field

--- bash/wordpress/infinatewp.sh
+++ bash/wordpress/infinatewp.sh
@@ -9,6 +9,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 php wp-cli.phar plugin install iwp-client --activate
 key=$(php wp-cli.phar option get iwp_client_activate_key)
 url=$(php wp-cli.phar option get siteurl)
+user=$(php wp-cli.phar user get 1 | grep user_login | awk '{print $2}')
 
 echo "Admin Url: $url/wp-admin"
 echo "Key:       $key"
+echo "User:      $user"
