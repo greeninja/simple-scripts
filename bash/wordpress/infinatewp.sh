3861df12e693743bada6ece96de6042a0d81786e
greeninja <nick@greeninja.co.uk>
2017-01-18 09:43:15 +0000

    changes

--- bash/wordpress/infinatewp.sh
+++ bash/wordpress/infinatewp.sh
@@ -3,7 +3,7 @@
 # Installs wp-cli as a php script, installs all tests from http://wptest.io and cleans up after itself.
 # Assumes web dir is /var/www/html
 
-# cd /var/www/html
+cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # Install import plugin
-php56-cli wp-cli.phar plugin install infinitewp-client --activate
+php wp-cli.phar plugin install infinitewp-client --activate
