bd2b35733f4eaf9296fb9ae17f32c92744fd4618
greeninja <nick@greeninja.co.uk>
2017-01-18 13:54:14 +0000

    add local script without defining web dir

---
+++ bash/wordpress/local_infinate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Installs wp-cli as a php script, installs all tests from http://wptest.io and cleans up after itself.
+# Assumes web dir is /var/www/html
+
+# cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# Install import plugin
+php wp-cli.phar plugin install iwp-client --activate
+key=$(php wp-cli.phar option get iwp_client_activate_key)
+url=$(php wp-cli.phar option get siteurl)
+user=$(php wp-cli.phar user get 1 | grep user_login | awk '{print $2}')
+
+echo "Admin Url: $url/wp-admin"
+echo "User:      $user"
+echo "Key:       $key"
